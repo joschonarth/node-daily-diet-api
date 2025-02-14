32e0d4b5dc471abb02a4a82eb896bf9ee2eb4c4b
joschonarth <joschonarth@gmail.com>
2025-02-14 18:13:39 -0300

    feat: create password hash and validation

--- src/controllers/register-controller.ts
+++ src/controllers/register-controller.ts
@@ -1,6 +1,8 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
 import z from 'zod'
+import { prisma } from '@/lib/prisma'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,12 +13,24 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { username, email, password } = registerBodySchema.parse(request.body)
 
+  const passwordHash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   const usersRepository = new UsersRepository()
 
   await usersRepository.create({
     username,
     email,
-    password,
+    password: passwordHash,
   })
 
   return reply.status(201).send()
